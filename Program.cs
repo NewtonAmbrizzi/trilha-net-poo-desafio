ca1e22eff6f631f784f04ee734f5c8655f8598f6
NewtonAmbrizzi <newton.ambrizzi@gmail.com>
2025-12-30 17:25:26 -0300

    feat: program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("123456", "iPhone 14", "111111", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
+
+Nokia nokia = new Nokia("654321", "Nokia Tijolão", "222222", 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
